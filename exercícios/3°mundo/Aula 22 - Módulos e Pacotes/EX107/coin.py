7a41fc7ed357a47fdee5d030f5fef1320116bf94
caroliny silva <caroliny.silv4@outlook.com>
2022-09-26 23:29:41 -0300

    organização das pastas

---
+++ exercícios/3°mundo/Aula 22 - Módulos e Pacotes/EX107/coin.py
@@ -0,0 +1,19 @@
+def aumentar(p, t):
+    res = p + (p * t/100)
+    return res
+
+
+def diminiuir (p, t):
+    res = p - (p * t/100)
+    return res
+
+
+def dobro(p):
+    res = p * 2
+    return res
+
+
+def metade(p):
+    res = p / 2 
+    return res
+    
---
+++ exercícios/3°mundo/Aula 22 - Módulos e Pacotes/EX107/teste.py
@@ -0,0 +1,5 @@
+import coin
+
+p = float(input('Digite p preço: R$'))
+print(f'A metade do preço é {coin.metade(p)}')
+print(f'A metade do preço é {coin.metade(p)}')
